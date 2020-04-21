1c449137c789a9b610d2983cac6b79dc3807181b
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-21 13:30:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
-# Add your code here
+class Dog 
+  attr_reader :name 
+  
+  @@all = []
+  
+  def initialize (name)
+    @name = name 
+    @@all << self
+  end
+  
+  def self.all
+    @@all 
+  end
+  
+  def self.print_all
+    @@all.each {|puppy| puts puppy.name}
+  end
+end
